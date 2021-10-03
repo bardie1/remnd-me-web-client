114bc03d75636ee8dc3de1082bc3bc2717feb8ca
saul bard <saulbard41@gmail.com>
2021-10-03 20:03:43 +1100

    fix router on base app

--- src/components/baseApp/BaseApp.tsx
+++ src/components/baseApp/BaseApp.tsx
@@ -1,33 +1,37 @@
 import {
-    BrowserRouter as Router,
+    BrowserRouter,
     Switch,
     Route,
-    Redirect
+    Redirect,
   } from "react-router-dom";
 import routes from "../../configuration/routes";
 import "./BaseApp.css";
 
 import * as React from 'react';
+import Header from "../header/Header";
 
 interface IBaseAppProps {
 }
 
 const BaseApp: React.FunctionComponent<IBaseAppProps> = (props) => {
   return (
-    <Router>
-        <Switch>
-            {
-                routes.map(route => {
-                    return (<Route key={route.path} path={route.path} exact={route.exact} component={route.component} />)
-                })
-            }
-            <Redirect from="/signup" to="/" />
-            <Redirect from="/login" to="/" />
-            <Route>
-                <div>NOT FOUND</div>
-            </Route>
-        </Switch>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Header />
+          <Switch>
+              {
+                  routes.map(route => {
+                      return (<Route key={route.path} path={route.path} exact={route.exact} component={route.component} />)
+                  })
+              }
+              <Redirect from="/signup" to="/" />
+              <Redirect from="/login" to="/" />
+              <Route>
+                  <div>NOT FOUND</div>
+              </Route>
+          </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
