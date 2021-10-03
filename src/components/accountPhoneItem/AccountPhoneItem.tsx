73126488bd6bbdcc31967423ff806cc8a3fc4b9c
saul bard <saulbard41@gmail.com>
2021-10-03 23:23:26 +1100

    create phone item component

---
+++ src/components/accountPhoneItem/AccountPhoneItem.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IAccountPhoneItemProps {
+}
+
+const AccountPhoneItem: React.FunctionComponent<IAccountPhoneItemProps> = (props) => {
+  return (
+      <div id="account-phone-item">
+          PhoneItem
+      </div>
+  );
+};
+
+export default AccountPhoneItem;
